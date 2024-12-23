462ce571e87bc508c9c530b20dcc1bc9b67cbdb0
Jason <kcliang0203@gmail.com>
2024-12-23 14:25:27 +0800

    feature:jason

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+void foo() {
+    printf("foo\n");
+}
 
 int main() {
     printf("Hello, World!\n");
     printf("Hello, World!!!!!!!!!!!!!!\n");
+    foo();
     return 0;
 }
