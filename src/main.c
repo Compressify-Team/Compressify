71467d5ce23529ba81673a7e72c811db0a9dabab
ya you <s112504506@g.ncu.edu.tw>
2024-12-31 20:25:09 +0800

    before merge main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <unistd.h>
+#include "arith_cod.h"
 
 #define MAX_CHAR 256
 
@@ -175,7 +178,7 @@ Node* deserializeTree(FILE* in_file) {
     }
 }
 
-void decompress_huffman(const char *file_content, size_t file_size, const char *input_file) {
+void decompress_huffman(const char *input_file) {
     // Open the compressed file for reading
     FILE *in_file = fopen(input_file, "rb");
     if (in_file == NULL) {
@@ -258,28 +261,9 @@ void decompress_huffman(const char *file_content, size_t file_size, const char *
     printf("Decompression complete. Output written to 'output_decoded.txt'.\n");
 }
 
-void arithmetic_compress(const char *file_content, size_t file_size, const char *input_file) 
-{
-    char output_file[256];
-    snprintf(output_file, sizeof(output_file), "%s.arc", input_file);
-    printf("Compressing %s to %s using Arithmetic Coding...\n", input_file, output_file);
-
-    // Open the output file for writing
-    FILE *out_file = fopen(output_file, "wb");
-    if (out_file == NULL) {
-        perror("Error opening output file");
-        return;
-    }
-
-    // TODO: Add Arithmetic Coding implementation here
-    fprintf(out_file, "Example compressed content (Arithmetic)\n");
-
-    // Close the output file
-    fclose(out_file);
-}
 
 // Function to perform Huffman compression
-int huffman_compress(const char *file_content, size_t file_size, const char *input_file) {
+int huffman_compress(const char *file_content, const char *input_file) {
     size_t len = strlen(file_content); 
     char output_file[256];
     snprintf(output_file, sizeof(output_file), "%s.huf", input_file);
@@ -384,106 +368,305 @@ int huffman_compress(const char *file_content, size_t file_size, const char *inp
     return compressed_size;
 }
 
-// Function to display usage information
-void show_help() 
-{
-    printf("Usage: ./main -c/-d [algorithm] [input_file]\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------arithmetic coding-------------------------------------------
+size_t calculate_output_size(const unsigned char *output, size_t max_size) {
+    size_t size = 0;
+    while (size < max_size && output[size] != '\0') {
+        size++;
+    }
+    return size;
 }
+
+void arithmetic_compress(const char *file_content, const char *input_file) {
+    size_t input_size = strlen(file_content);
+
+    size_t output_size = input_size * 2; // 預估大小
+
+    unsigned char* input = (unsigned char*)file_content;
+    unsigned char* output = calloc(sizeof(unsigned char) * output_size, 1);
+
+    // initialize the encoder state
+    ac_state_t encoder_state;
+    init_state(&encoder_state, 16);
+
+    // build the probability table
+    build_probability_table(&encoder_state, input, input_size);
+
+    printf("Encoding...\n");
+
+    encode_value(output, input, input_size, &encoder_state);
+
+
+    char output_file[256];
+
+    snprintf(output_file, sizeof(output_file), "%s.arc", input_file);
+    FILE *out_file = fopen(output_file, "wb");
+    if (out_file == NULL) {
+        perror("Error opening output file");
+        free(output);
+        return;
+    }
+    
+
+    size_t actual_output_size = calculate_output_size(output, output_size);
+
+    fwrite(&input_size, sizeof(size_t), 1, out_file);
+    printf("input_size: %zu\n", input_size);
+
+    fwrite(encoder_state.prob_table, sizeof(int), 256, out_file); 
+    fwrite(encoder_state.cumul_table, sizeof(int), 257, out_file); 
+    fwrite(&encoder_state.frac_size, sizeof(int), 1, out_file);
+    fwrite(&encoder_state.one_counter, sizeof(int), 1, out_file);
+    fwrite(&encoder_state.current_index, sizeof(int), 1, out_file);
+    fwrite(&encoder_state.out_index, sizeof(int), 1, out_file);
+    fwrite(&encoder_state.last_symbol, sizeof(int), 1, out_file);
+    fwrite(&encoder_state.current_symbol, sizeof(unsigned char), 1, out_file);
+    fwrite(&encoder_state.base, sizeof(int), 1, out_file);
+    fwrite(&encoder_state.length, sizeof(int), 1, out_file);
+
+    fwrite(output, sizeof(unsigned char), output_size, out_file);
+
+    printf("output cotent: %s\n", output);
+    printf("output_size: %zu\n", actual_output_size);
+    /*
+    //print all encoder_state
+    printf("ac_state_t:\n");
+    printf("  frac_size: %d\n", encoder_state.frac_size);
+    printf("  one_counter: %d\n", encoder_state.one_counter);
+    printf("  current_index: %d\n", encoder_state.current_index);
+    printf("  out_index: %d\n", encoder_state.out_index);
+    printf("  last_symbol: %d\n", encoder_state.last_symbol);
+    printf("  current_symbol: %c\n", encoder_state.current_symbol);
+    printf("  base: %d\n", encoder_state.base);
+    printf("  length: %d\n", encoder_state.length);
+*/
+    fclose(out_file);
+    free(output);
+
+}
+
+
+
+void arithmetic_decompress(const char *input_file) {
+
+    ac_state_t encoder_state;
+    init_state(&encoder_state, 16);
+
+    printf("Decompressing %s using Arithmetic Coding...\n", input_file);
+
+    FILE *in_file = fopen(input_file, "rb");
+    if (in_file == NULL) {
+        perror("Error opening input file");
+        return;
+    }
+
+    size_t input_size;
+    fread(&input_size, sizeof(size_t), 1, in_file);
+
+
+    encoder_state.prob_table = malloc(sizeof(int) * 256); 
+    encoder_state.cumul_table = malloc(sizeof(int) * 257); 
+
+
+    fread(encoder_state.prob_table, sizeof(int), 256, in_file);
+    fread(encoder_state.cumul_table, sizeof(int), 257, in_file);
+    fread(&encoder_state.frac_size, sizeof(int), 1, in_file);
+    fread(&encoder_state.one_counter, sizeof(int), 1, in_file);
+    fread(&encoder_state.current_index, sizeof(int), 1, in_file);
+    fread(&encoder_state.out_index, sizeof(int), 1, in_file);
+    fread(&encoder_state.last_symbol, sizeof(int), 1, in_file);
+    fread(&encoder_state.current_symbol, sizeof(unsigned char), 1, in_file);
+    fread(&encoder_state.base, sizeof(int), 1, in_file);
+    fread(&encoder_state.length, sizeof(int), 1, in_file);
+
+    size_t output_size = input_size*2; 
+    unsigned char* output = calloc(sizeof(unsigned char) * output_size,1);
+    unsigned char* decomp = calloc(sizeof(unsigned char) * input_size,1); 
+
+    fread(output, sizeof(unsigned char), output_size , in_file);
+
+    fclose(in_file);
+    /*
+    printf("Input size: %zu\n", input_size);
+    printf("ac_state_t2:\n");
+    printf("  frac_size: %d\n", encoder_state.frac_size);
+    printf("  one_counter: %d\n", encoder_state.one_counter);
+    printf("  current_index: %d\n", encoder_state.current_index);
+    printf("  out_index: %d\n", encoder_state.out_index);
+    printf("  last_symbol: %d\n", encoder_state.last_symbol);
+    printf("  current_symbol: %c\n", encoder_state.current_symbol);
+    printf("  base: %d\n", encoder_state.base);
+    printf("  length: %d\n", encoder_state.length);
+    printf("cumul %d",encoder_state.cumul_table[256]);
+    printf("output cotent: %s\n", output);
+    */
+    
+    printf("Decoding...\n");
+
+    decode_value(decomp, output, &encoder_state, input_size);
+    printf("Decoded string: %s\n", decomp);
+
+    char output_file[256];
+    strncpy(output_file, input_file, sizeof(output_file) - 1);
+    output_file[sizeof(output_file) - 1] = '\0';
+
+    // 去掉 .txt.huf 
+    char *dot = strrchr(output_file, '.');
+    if (dot && strcmp(dot, ".huf") == 0) {
+        *dot = '\0';
+        dot = strrchr(output_file, '.');
+        if (dot && strcmp(dot, ".txt") == 0) {
+            *dot = '\0';
+        }
+    }
+
+    strncat(output_file, "_arithmetic.txt", sizeof(output_file) - strlen(output_file) - 1);
+
+    // 寫檔
+    FILE *out_file = fopen(output_file, "wb");
+    if (out_file == NULL) {
+        perror("Error opening output file");
+        free(output);
+        free(decomp);
+        free(encoder_state.prob_table);
+        free(encoder_state.cumul_table);
+        return;
+    }
+    fwrite(decomp, 1, input_size, out_file);
+    fclose(out_file);
+    printf("Decoded content written to %s\n", output_file);
+
+    free(output);
+    free(decomp);
+}
+
+//-------------------------------------------arithmetic coding end-------------------------------------------
+
+
+// Function to display input prompt
+void show_prompt()
+{
+    printf("Please enter the decompression/compression algorithm and input file:\n");
+    printf("Algorithms can be used: huffman, arithmetic\n");
+    printf("Usage: -c/-d [algorithm] [input_file]\n");
+}
+
 
 // Main function
-int main(int argc, char *argv[]) {
-    // Check if the correct number of arguments is provided
-    if (argc != 4) 
+int main() 
+{
+    while (1)
     {
-        show_help();
-        return 1;
-    }
+        char com_or_decom[100];
+        char algorithm[100];
+        char input_file[100];
+        // Display input prompt
+        show_prompt();
 
-    const char *mode = argv[1];
-    const char *algorithm = argv[2];
-    const char *input_file = argv[3];
+        // Read user input
+        if (scanf("%99s %99s %99s", com_or_decom, algorithm, input_file) != 3) {
+            return 1;
+        }
 
-    // Open the input file for reading
-    FILE *file = fopen(input_file, "rb");
-    if (file == NULL) {
-        perror("Error opening input file");
-        return 1;
-    }
 
-    // Determine the file size
-    fseek(file, 0, SEEK_END);
-    size_t file_size = ftell(file);
-    rewind(file);
-    //store the input file size
-    printf("Input file size: %zu bytes\n", file_size);
+        if (strcmp(com_or_decom,"-c"))
+        {
+            printf("Compress or Decompress: compress\n");
+        }
+        else if (strcmp(com_or_decom,"-d"))
+        {
+            printf("Compress or Decompress: decompress\n");
+        }
+        else
+        {
+            printf("Error: Unknown operation '%s'. Use -c for compression or -d for decompression.\n", com_or_decom);
+            return 1;
+        }
+        printf("Algorithm: %s\n", algorithm);
+        printf("Input file: %s\n", input_file);
 
-    // Allocate memory to hold the file content
-    char *file_content = (char *)malloc(file_size);
-    if (file_content == NULL) {
-        perror("Memory allocation failed");
+
+
+        // Open the input file for reading
+        FILE *file = fopen(input_file, "rb");
+        if (file == NULL) {
+            perror("Error opening input file");
+            return 1;
+        }
+
+        // Determine the file size
+        fseek(file, 0, SEEK_END);
+        size_t file_size = ftell(file);
+        rewind(file);
+
+        // Allocate memory to hold the file content
+        char *file_content = (char *)calloc(file_size, 1);
+        if (file_content == NULL) {
+            perror("Memory allocation failed");
+            fclose(file);
+            return 1;
+        }
+
+        // Read the file content
+        fread(file_content, 1, file_size, file);
         fclose(file);
-        return 1;
-    }
 
-    // Read the file content
-    fread(file_content, 1, file_size, file);
-    fclose(file);
-
-    // Select the appropriate operation and compression algorithm
-    if (strcmp(mode, "-c") == 0) 
-    {
-        if (strcmp(algorithm, "huffman") == 0) 
-        {
-            int compressed_size = huffman_compress(file_content, file_size, input_file);
-            if(compressed_size != 0){
-                //calculate the compression ratio
-                printf("Compressed file size: %d bytes\n", compressed_size);
-                double compression_ratio = (double)compressed_size / (double)file_size;
-                printf("Compression ratio: %.2f\n", compression_ratio);
+        // Select the appropriate action based on user input
+        if (strcmp(com_or_decom, "-c") == 0) {
+            // Compression
+            if (strcmp(algorithm, "huffman") == 0) {
+                int compressed_size = huffman_compress(file_content, input_file);
+                if(compressed_size != 0){
+                    printf("Compressed file size: %d bytes\n", compressed_size);
+                    double compression_ratio = (double)compressed_size / (double)file_size;
+                    printf("Compression ratio: %.2f\n", compression_ratio);
+                }
+            } else if (strcmp(algorithm, "arithmetic") == 0) {
+                arithmetic_compress(file_content, input_file);
+            } else {
+                printf("Error: Unknown algorithm '%s'.\n", algorithm);
+                free(file_content);
+                return 1;
             }
-        } 
-        else if (strcmp(algorithm, "arithmetic") == 0) 
-        {
-            arithmetic_compress(file_content, file_size, input_file);
-        } 
-        else 
-        {
-            printf("Error: Unknown algorithm '%s'.\n", algorithm);
-            show_help();
+        } else if (strcmp(com_or_decom, "-d") == 0) {
+            // Decompression
+            if (strcmp(algorithm, "huffman") == 0) {
+                printf("Decompressing %s using Huffman Coding...\n", input_file);
+                decompress_huffman(input_file);
+                
+            } else if (strcmp(algorithm, "arithmetic") == 0) {
+                arithmetic_decompress(input_file);
+            } else {
+                printf("Error: Unknown algorithm '%s'.\n", algorithm);
+                free(file_content);
+                return 1;
+            }
+        } else {
+            printf("Error: Unknown operation '%s'. Use -c for compression or -d for decompression.\n", com_or_decom);
             free(file_content);
             return 1;
         }
-    } 
-    else if (strcmp(mode, "-d") == 0) 
-    {
-        if (strcmp(algorithm, "huffman") == 0) 
-        {
-            // TODO: Add Huffman decompression implementation here
-            printf("Decompressing %s using Huffman Coding...\n", input_file);
-            decompress_huffman(file_content, file_size, input_file);
-        } 
-        else if (strcmp(algorithm, "arithmetic") == 0) 
-        {
-            // TODO: Add Arithmetic decompression implementation here
-            printf("Decompressing %s using Arithmetic Coding...\n", input_file);
-        } 
-        else 
-        {
-            printf("Error: Unknown algorithm '%s'.\n", algorithm);
-            show_help();
-            free(file_content);
-            return 1;
-        }
-        } 
-        else 
-        {
-            printf("Error: Unknown mode '%s'.\n", mode);
-            show_help();
-            free(file_content);
-            return 1;
-        }
-    // Free allocated memory
-    free(file_content);
 
+        // Free allocated memory
+        free(file_content);
+        printf("\n\n\n");
+
+    }
+    
     return 0;
 }
+
+
